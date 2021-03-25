7b53310a29e317fbda0b66fa1f670c202ad63cbb
Trynda-LOL <bm33518@gmail.com>
2021-03-25 10:06:46 -0300

    component ExperienceBar

--- movue-it-nuxt/layouts/default.vue
+++ movue-it-nuxt/layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
-  <Nuxt />
+	<div class="min-h-screen max-w-5xl mx-auto flex flex-col px-5 py-8 md:py-10">
+		<ExperienceBar />
+		<Nuxt />
+	</div>
 </template>
 
-<style>
+<script lang="ts">
+import Vue from 'vue';
+import ExperienceBar from '~/components/atoms/ExperienceBar.vue';
 
-</style>
+export default Vue.extend({
+	name: 'Default',
+	components: {	ExperienceBar	}
+});
+</script>
